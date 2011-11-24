31740c3435e19d45c8acae92ff50948051c198e5
Erik Aasmul <erik.aasmul@gmail.com>
2011-11-24 20:40:43 +0100

    xml for personliste

--- Hvem-Kommer-Play-Java/app/controllers/Persons.java
+++ Hvem-Kommer-Play-Java/app/controllers/Persons.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import models.Person;
 import models.Trening;
 import play.data.validation.Required;
+import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
 import play.mvc.Controller;
 import play.*;
 import play.mvc.*;
@@ -13,4 +16,9 @@ import java.util.*;
 
 public class Persons extends CRUD {
 
+  public static void listXml() {
+    List<Person> persons = Person.findAll();
+    Logger.info("er innne i listXml. Antall personer=" + persons.size());
+    render(persons);
+  }
 }
