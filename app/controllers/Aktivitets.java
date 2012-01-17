0402d889d5ab086d9e0dbb2fb03e8c0783b323f3
Bjorn Hamre <javaguruen@gmail.com>
2012-01-17 23:06:04 +0100

    Lagt header ut i main.html som blir inkludert fra viewet.

--- app/controllers/Aktivitets.java
+++ app/controllers/Aktivitets.java
@@ -1,9 +1,24 @@
 package controllers;
 
 
+import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
+
 public class Aktivitets extends CRUD {
     /*@Before
     public static void addFilter() {
         request.args.put("where", "aktiv=true");
     }*/
+    public static void show(String id) {
+        ObjectType type = ObjectType.get(getControllerClass());
+        notFoundIfNull(type);
+        Model object = type.findById(id);
+        notFoundIfNull(object);
+        try {
+            render(type, object);
+        } catch (TemplateNotFoundException e) {
+            render("CRUD/show.html", type, object);
+        }
+    }
+
 }
