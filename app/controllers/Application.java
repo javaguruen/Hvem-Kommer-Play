f9ab32dc4addde2cb9303064c1cc2e1ee82f60f8
Erik Aasmul <erik.aasmul@gmail.com>
2012-01-19 16:33:36 +0100

    Flyttet påmelding til Deltakelses-kontroller + pyntet på siden.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,69 +19,6 @@ public class Application extends Controller {
     render(grupper, aktiviteter);
   }
 
-
-  public static void paamelding(String gruppeNavn, Long aktivitetId) {
-    Gruppe gruppe = Gruppe.finnByNavn(gruppeNavn);
-    if (gruppe==null) {
-      notFound("Fant ikke gruppe " +gruppeNavn);
-    }
-    List<Aktivitet> aktiviteter = Aktivitet.finnAlleAktive(gruppe.id);
-    if (aktiviteter.size()==0) {
-      notFound("Fant ikke aktiviteter for " +gruppeNavn);
-    }
-    if (aktivitetId==null) {
-      aktivitetId = finnNesteAktivitetId(aktiviteter);
-    }
-
-    Aktivitet gjeldendeAktivitet = Aktivitet.findById(aktivitetId);
-    List<Deltakelse> deltakelserKommer = Deltakelse.finnAlleSomKommer(aktivitetId);
-    List<Deltakelse> deltakelserKommerIkke = Deltakelse.finnAlleSomIkkeKommer(aktivitetId);
-    List<Person> personerUtenStatus = Person.finnAlleUtenStatus(aktivitetId);
-
-    renderTemplate("Application/paamelding.html",gruppe,aktiviteter, deltakelserKommer, deltakelserKommerIkke, personerUtenStatus, gjeldendeAktivitet);
-  }
-
-  public static void slettPaamelding(Long deltakelseId) {
-    Deltakelse deltakelse = Deltakelse.findById(deltakelseId);
-    String aktivitetId = deltakelse.aktivitet.getId().toString();
-    deltakelse.delete();
-    redirect("Application.paamelding", deltakelse.aktivitet.gruppe.navn,aktivitetId);
-  }
-
-  public static void settStatus(@Required Long personId, String status, Long aktivitetId) {
-    Aktivitet aktivitet = Aktivitet.findById(aktivitetId);
-    Logger.info(personId +", " + status + ", " + aktivitetId);
-    if (personId == null) {
-      redirect("Application.paamelding", aktivitet.gruppe.navn,aktivitetId);
-    }
-    Deltakerstatus deltakerstatus = Deltakerstatus.valueOf(status);
-    Person person = Person.findById(personId);
-    Deltakelse deltakelse = Deltakelse.finn(person, aktivitet);
-    if (deltakelse == null) {
-      deltakelse = new Deltakelse(person, aktivitet);
-    }
-
-    deltakelse.status = deltakerstatus;
-    deltakelse.save();
-    redirect("Application.paamelding", aktivitet.gruppe.navn,aktivitetId);
-  }
-
-
-  public static void endreAktivAktivitet(@Required Long aktivitetId) {
-    Logger.info("Ny aktivitetId: " + aktivitetId);
-    Aktivitet aktivitet = Aktivitet.findById(aktivitetId);
-    redirect("Application.paamelding", aktivitet.gruppe.navn,aktivitetId);
-  }
-
-  private static Long finnNesteAktivitetId(List<Aktivitet> aktiviteter) {
-    if (aktiviteter.size() == 0) {
-      return null;
-    } else {
-      Aktivitet nesteAktivitet = aktiviteter.get(0);
-      return nesteAktivitet.getId();
-    }
-  }
-
   public static void listDeltakelser(Long aktivitetId) {
     //Finn alle deltakelser for gitte aktivitet
     List<Deltakelse> deltakelser = Deltakelse.find("aktivitet.id=?", aktivitetId).fetch();
